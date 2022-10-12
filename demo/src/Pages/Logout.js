270d1cc60604dd75ca9f235e825f2fd521f8a056
Shivani Sathyanarayan <Shivani.S@ivycomptech.com>
2022-10-12 20:01:11 +0530

    react ques

---
+++ demo/src/Pages/Logout.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import{useNavigate} from "react-router-dom";
+
+export const Logout=()=>{
+    const navigate=useNavigate();
+    const logout=()=>{
+        localStorage.setItem('login',false)
+        localStorage.clear()
+        navigate('/Login')
+    }
+   
+ 
+
+    return(
+        <div>
+            <button onClick={logout}>LogOut</button>
+        </div>
+    )
+}
+export default Logout;
